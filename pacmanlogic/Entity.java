0a1e692f87e8b264d5b02241e92c2b1d73998046
VivilT <47364767+VivilT@users.noreply.github.com>
2019-04-11 12:16:52 -0600

    Working

---
+++ pacmanlogic/Entity.java
@@ -0,0 +1,10 @@
+package pacmanlogic;
+
+public abstract class Entity{
+    public abstract int[] move(String input);
+
+    public abstract void setPosition(int[] position);
+
+    public abstract int[] getPosition();
+    
+}
